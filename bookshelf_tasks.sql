641397a38c6ea05918c7ba1e7ae8f788126f7515
leaderofthejuicers <d9157402222@mail.ru>
2025-05-26 15:21:25 +0300

    Update bookshelf_tasks.sql

--- bookshelf_tasks.sql
+++ bookshelf_tasks.sql
@@ -53,3 +53,28 @@ left join authors_books on books.id = authors_books.books_id
 left join authors on authors_books.authors_id = authors.id
 where shelves.title like '%верхняя%' or shelves.title like '%нижняя%';
 
+-- 8. 
+select id into @friend_id from friends where name like "Иванов Иван";
+
+select books.id into @book_id from books
+join authors_books on books.id = authors_books.books_id
+join authors on authors_books.authors_id = authors.id
+where title like "Божественная комедия" and authors.name like "Данте Алигьери";
+
+update books 
+set friends_id = @friend_id
+where id = @book_id;
+
+
+select * from books 
+join authors_books on books.id = authors_books.books_id
+join authors on authors_books.authors_id = authors.id
+where title like "Божественная комедия";
+
+
+-- 9. 
+select id into @shelf_id from shelves where title = 'Полка в кабинете';
+insert into authors (name) values ('Стивен Хокинг');
+insert into books (title, year, shelves_id) values ('Краткие ответы на большие вопросы', 2020, @shelf_id);
+SELECT LAST_INSERT_ID() into @book_id;
+insert into authors_books (books_id, authors_id) values (@book_id, @author_id);
