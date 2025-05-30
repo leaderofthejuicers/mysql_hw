bda5608dc42130085b1181bfaee0d2aba21fc1ea
leaderofthejuicers <d9157402222@mail.ru>
2025-05-30 04:57:30 +0300

    Create movies_tasks.sql

---
+++ movies_tasks.sql
@@ -0,0 +1,38 @@
+-- 1. 
+select count(*) from movies 
+join companies on movies.companies_id = companies.id
+where companies.title = "Universal Pictures";
+
+-- 2. 
+select count(*) from movies 
+join directors on movies.directors_id = directors.id
+where directors.full_name = "Фрэнсис Форд Коппола";
+
+-- 3. 
+select count(*) from movies 
+where year > (2025 - 20);
+
+-- 4. 
+select distinct genres.title from genres 
+join movies on genres.id = movies.genres_id
+join directors on movies.directors_id = directors.id 
+where directors.full_name = "Стивен Спилберг";
+
+-- 5. 
+select movies.title, genres.title genre, directors.full_name director from movies 
+join directors on movies.directors_id = directors.id
+join genres on movies.genres_id = genres.id
+order by budget
+limit 5;
+
+-- 6.
+select full_name, count(movies.id) from movies
+join directors on  movies.directors_id = directors.id 
+group by directors.full_name
+order by count(movies.id) desc
+limit 1;
+ 
+-- 7. 
+-- 8. 
+-- 9. 
+-- 10. 
